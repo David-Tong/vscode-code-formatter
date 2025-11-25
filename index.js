5f43a77f0d72aea5ca7f409c9fc4d60e5838c406
David Tong <ytong@ebay.com>
2025-11-25 05:36:14 -0600

    Init Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const a = 31;
+
+const b = 42;
+
+const c = "Another constant";
+
+console.log("This is bad, very bad");
+console.log("nice");
+
+function foo(a) {
+    if (a > 10) {
+        return a * 2;
+    }
+    return a + 2;
+}
+
+console.log(foo(5));
+console.log(foo(15));
+console.log("End of the script");
